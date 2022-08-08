2710a01930ddea91b677eaa304516d40b5a8e89d
JohnZ <cxyzhang@gmail.com>
2022-08-07 22:03:08 -0700

    modified:   main.go 	../go.mod 	../go.sum

--- examples/main.go
+++ examples/main.go
@@ -13,7 +13,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	list, err := b.Token("{Access token}").Coin("tbtc").Debug(true).ListWallets(bitgo.ListParams{
+	list, err := b.Token("b5917623a6bc3a49fc40f3b7f25d6ddf49f1992d0e46ed3f37c5e029d46601d2").Coin("tbtc").Debug(true).ListWallets(bitgo.ListParams{
+	// list, err := b.Token("{Access token}").Coin("tbtc").Debug(true).ListWallets(bitgo.ListParams{
 		AllTokens: true,
 	})
 	if err != nil {
